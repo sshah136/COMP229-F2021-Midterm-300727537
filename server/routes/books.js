0d871880d2c97d9c0745bfc4af22a7bc49a85f2b
sshah136 <sshah136@my.centennialcollege.ca>
2021-11-02 17:55:40 -0400

    Routing logic for Add,Edit and Delete

--- server/routes/books.js
+++ server/routes/books.js
@@ -23,47 +23,105 @@ router.get('/', (req, res, next) => {
 
 });
 
+router.get('/add', (req, res, next) => {
+  res.render('books/details', {
+      title: 'Add a Book',
+      books: ''
+  });
+});
+
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    //Pass an appropriate value for the title property and blank value for the books property
+    res.render('books/details', { title: 'Add a Book' });
 
 });
 
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    //Instantiate an object of the book model and pass this object to the create method of the book model to add a new book to the database
+    let books = book({
+      "Title": req.body.title,
+      "Price": req.body.price,
+      "Author": req.body.author,
+      "Genre": req.body.genre
+  });
+  
+  book.create(books, (err, book) => {
+      if (err) {
+          console.log(err);
+          res.end(err);
+      }
+  })
+
+  console.log(books);
+  // Redirect the user back to the BookList page ('/books') when the insertion is completed
+  res.redirect('/books');
 
 });
 
 // GET the Book Details page in order to edit an existing Book
-router.get('/:id', (req, res, next) => {
+router.get('/edit/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    //Declare an id variable and set its value to the id property of the request object
+    let id = req.params.id;
+
+    //Pass this id to the book model’s findById method to render the book details view
+    book.findById(id, (err, bookEdit) => {
+        if (err) {
+            console.log(err);
+            res.end(err);
+        }
+
+        //Set an appropriate title property value and set the books property to the book that was returned from the database as you render the view
+        res.render('books/details', { title: 'Edit Books', books: bookEdit });
+    })
 });
 
 // POST - process the information passed from the details form and update the document
-router.post('/:id', (req, res, next) => {
+router.post('/edit/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    // Declare an id variable and set its value to the id property of the request object
+    let id = req.params.id;
+
+    // Instantiate an object of the book model and pass this object to the update method of the book model to edit an existing book in the database
+    let updatedBook = book({
+        "_id": id,
+        "Title": req.body.title,
+        "Price": req.body.price,
+        "Author": req.body.author,
+        "Genre": req.body.genre
+    });
+
+    book.updateOne({ _id: id }, updatedBook, (err) => {
+        if (err) {
+            console.log(err);
+            res.end(err);
+        }
+
+        //Redirect the user back to the BookList page when the update is completed
+        res.redirect('/books'); 
+    });
 
 });
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    let id = req.params.id;//Declare an id variable and set its value to the id property of the request object
+
+    //Pass the id to the book model’s remove method
+    book.remove({ _id: id }, (err) => {
+        if (err) {
+            console.log(err);
+            res.end(err);
+        }
+
+        res.redirect('/books');//Redirect the user back to the BookList page when the removal is completed
+    });
+    
 });
 
 
